c1909e7c00179fee790352bd74e0f72eee2a4002
“653410028” <653410028@qq.com>
2022-05-13 10:32:57 +0800

    Initial commit

---
+++ src/com/itxuexi/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.itxuexi;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("Ctrl + Alt + Space = 内容辅助键");
+
+//        System.out.println("HelloWorld");
+//        System.out.println("Ctrl + Alt + Space = 内容辅助键");
+
+/*        System.out.println("HelloWorld");
+        System.out.println("Ctrl + Alt + Space = 内容辅助键");*/
+
+        int i = 1;
+        System.out.println("i=" + i);
+    }
+}
---
+++ src/com/itxuexi/ShuJu.java
@@ -0,0 +1,27 @@
+package com.itxuexi;
+
+public class ShuJu {
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+        int[] arr1 = new int[2];
+/*        System.out.println(arr);
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr1);
+        System.out.println(arr1[0]);
+        System.out.println(arr1[1]);*/
+        arr[0] = 100;
+        arr[1] = 200;
+        arr[2] = 300;
+        System.out.println(arr);
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        arr1[0] = 111;
+        arr1[1] = 222;
+        System.out.println(arr1);
+        System.out.println(arr1[0]);
+        System.out.println(arr1[1]);
+    }
+}
